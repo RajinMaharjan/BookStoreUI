ef6fc872bd48a3fc0b30dce76ad90d555945533f
RajinMaharjan <mjnrajin.18@gmail.com>
2023-10-08 08:41:52 +0545

    Css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { BookListComponent } from './features/book/book-list/book-list.component';
+import { AddBookComponent } from './features/book/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BookListComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
